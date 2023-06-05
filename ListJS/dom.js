79ba7eeb83a7780b8e0dbeaf0ffa6e69442db690
gedeamerta <amerta213bali@gmail.com>
2023-06-05 18:52:41 +0800

    Updated for todo list

--- ListJS/dom.js
+++ ListJS/dom.js
@@ -10,7 +10,53 @@ DOM Tipe Data:
 
 */
 console.info("----------------------- DOM --------------------------------")
+
+
+// Node 
 let fruits = document.getElementById("fruits");
 console.info(fruits);
 console.info(fruits.children);
 console.info(fruits.lastChild);
+
+// Element
+let creds = document.getElementById("information");
+const firstName = document.createElement("li");
+const lastName = document.createElement("li");
+firstName.textContent = "Gede"
+lastName.textContent = "Amerta"
+creds.appendChild(firstName);
+creds.appendChild(lastName);
+
+// Nodelist -> ada nodelist Static dan Live
+// definisi Nodelist yg Static dan Live bisa dilihat dari query atau getelement nya. for more details go to Udemy 
+let nameofInfoUser = document.querySelectorAll("li");
+console.log(nameofInfoUser);
+
+let childName = document.getElementById("info-user").childNodes;
+console.log(childName);
+
+// Attr
+let getPId = document.getElementById("add-attribute");
+let setAttribute = getPId.setAttribute("class", "this-is-new-attribute");
+
+// NameNodeMap -> kumpulan dari attribute
+let getAttributeNode = getPId.attributes;
+for (const attribute of getAttributeNode) {
+    console.log(`${attribute.name}: ${attribute.value}`)
+}
+
+
+// Event Handler
+
+// Event Target
+// let clickID = document.getElementById("event-handler-id");
+// clickID.addEventListener("click", function(){
+//     clickID.setAttribute("value", "you are already click me");
+// })
+
+// Global Event
+let clickID = document.getElementById("event-handler-id");
+clickID.onclick = function() {
+    console.log("you click the button")
+}
+// Bedanya Event Target & Global Event adalah penggunaannya, EVent Target bisa beberapa menambahkan beberapa kali callback. kalau pake GLobal Event hanya bisa sekali callback
