cf69828a03a058845458a33d7877f379772aa3fa
gedeamerta <amerta213bali@gmail.com>
2023-06-22 00:28:19 +0800

    update

--- ListJS/basic-js-module/script/withModule.mjs
+++ ListJS/basic-js-module/script/withModule.mjs
@@ -1,3 +1,4 @@
+//  One Export
 export class Person {
     constructor (personName) {
         this.personName = personName;
@@ -5,6 +6,10 @@ export class Person {
 
     sayHelloClass(personName) {
         console.info(`Hello Class my name is ${this.personName}`);
+    }
+
+    sumNum(a,b) {
+        console.info(`${a + b}`);
     }
 }
 
@@ -17,3 +22,41 @@ export function shoutMyName(name) {
 }
 
 export let myName = "Gede Amerta";
+
+// Multiple Export
+class Car {
+    constructor (carName) {
+        this.carName = carName;
+    }
+
+    repairCar(ourMoney, price) {
+        let total = ourMoney - price;
+        console.info(`I have money ${ourMoney} i need to repair my car with this price: ${price}`);
+        console.info(`${this.carName}, Cost: ${total}`);
+    }
+}
+
+function carDealer(list) {
+    console.info(`Here are Car Dealer lists: ${list}`);
+}
+
+export {Car, carDealer};
+
+// Alias: better using alias while importing the function or whatever things to export
+class Car2 {
+    constructor (carName) {
+        this.carName = carName;
+    }
+
+    repairCar2(ourMoney, price) {
+        let total = ourMoney - price;
+        console.info(`I have money ${ourMoney} i need to repair my car with this price: ${price}`);
+        console.info(`${this.carName}, Cost: ${total}`);
+    }
+}
+
+function carDealer2(list) {
+    console.info(`Here are Car Dealer lists: ${list}`);
+}
+
+export {Car2, carDealer2};
