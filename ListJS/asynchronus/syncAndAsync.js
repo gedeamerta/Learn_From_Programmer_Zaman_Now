4f49d67c70565fc0e3dc61d79753c8099ef47fb5
gedeamerta <amerta213bali@gmail.com>
2023-06-29 16:57:33 +1000

    update

---
+++ ListJS/asynchronus/syncAndAsync.js
@@ -0,0 +1,5 @@
+// Synchronus: code yang akan di eksekusi baris per baris
+// Blocking: menunggu proses baris pertama selesai baru bisa eksekusi di baris selanjutnya
+
+// Asynchronus: code yang akan di eksekusi tanpa harus menunggu baris pertama selesai.
+// Non blocking: tanpa menunggu baris pertama selesai di eksekusi bisa menuju ke baris kedua
