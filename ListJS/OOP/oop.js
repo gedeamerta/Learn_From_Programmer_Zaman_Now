15f46ea38d73c1c655c8cc4d9dd91db405883903
gedeamerta <amerta213bali@gmail.com>
2024-07-05 00:44:55 +1000

    OOP

---
+++ ListJS/OOP/oop.js
@@ -0,0 +1,7 @@
+// Object Oriented Programming adalah sudut pandang bahasa pemrograman yang berkonsep "object"
+// Istilah yang perlu di perhatikan yaitu: Object dan Class
+// Object => data berisikan field / properties / attributes dan method / function / behavior
+
+// Class adalah blueprint, prototype atau cetakan untuk membuat Object.
+// Class berisikan deklarasi semua property atau cetakan untuk membuat Object.
+
---
+++ ListJS/asynchronus/ajax/ajax.js
@@ -0,0 +1,8 @@
+/*
+Ajax: mengirim data ke server atau menerima data dari server
+
+kita tidak dapat langsung mengambil response ajax, karena proses ajax adalah Async. kita perlu menunggu sampai proses Async selesai.
+
+
+
+*/
